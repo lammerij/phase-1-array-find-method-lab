e59ade3ecb62e78f5e95d2ae76a469c729c74586
+ <+>
2022-02-09 11:51:51 -0500

    commit tree 0fdf9fb9a47f3db0269a9095f991098287e11b60

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(array) {
+  //console.log(array);
+  //array.find((obj) => obj.result === "W");
+  //console.log(array.find((obj) => obj.result === "W"));
+  let foundRecord = array.find((obj) => obj.result === "W");
+  console.log(typeof foundRecord);
+  console.log(typeof {});
+  if (typeof foundRecord === "undefined") {
+    return foundRecord;
+  } else if (typeof foundRecord === typeof {}) {
+    return foundRecord.year;
+  }
+}
